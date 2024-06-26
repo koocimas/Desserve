3a5aeca556334a3a1c7f0acd70dce607d8df9a66
Sam Cook <129230960+koocimas@users.noreply.github.com>
2024-06-26 18:05:51 -0400

    Initial Commit

---
+++ Desserve/DesserveApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesserveApp.swift
+//  Desserve
+//
+//  Created by Sam Cook on 6/26/24.
+//
+
+import SwiftUI
+
+@main
+struct DesserveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
